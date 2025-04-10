a121b5c979cac4a9ae275a918c3c4a65b7e41406
amal-552004 <chaaibiamal05@gmail.com>
2025-04-10 17:58:01 +0200

    routes.test.js

--- backend/tests/integration/routes.test.js
+++ backend/tests/integration/routes.test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../server'); // Corrigé ici
+const app = require('../../server'); // Corrigé ici
 
 chai.use(chaiHttp);
 const { expect } = chai;

497d2adc6add31444f370ca3f27487619c3e6f3e
amal-552004 <chaaibiamal05@gmail.com>
2025-04-10 17:20:37 +0200

    Test d’intégration d’une route

---
+++ backend/tests/integration/routes.test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../index'); // Assure-toi que ton app peut être importée
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('GET /api/users', () => {
+  it('should return status 200 and a list of users', (done) => {
+    chai.request(app)
+      .get('/api/users')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        done();
+      });
+  });
+});

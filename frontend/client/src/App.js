7c816df9ef2b1e9a67db2ca09ee52148c576b3b8
amal-552004 <chaaibiamal05@gmail.com>
2025-04-10 03:12:32 +0200

    App.js

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import UserList from './compenents/UserList';  // On va importer ton composant UserList
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Gestion des Utilisateurs</h1>
+      <UserList /> {/* On ajoute notre composant ici */}
     </div>
   );
 }
